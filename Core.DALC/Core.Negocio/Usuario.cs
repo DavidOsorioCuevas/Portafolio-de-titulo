f7ab1aaac526b8997b75953513b4050a2b59f5b3
DavidOsorioCuevas <dav.osorio@alumnos.duoc.cl>
2017-10-01 23:59:53 -0300

    primer commit

---
+++ Core.DALC/Core.Negocio/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Negocio
+{
+    public class Usuario
+    {
+
+
+        public string NOMBRE_USUARIO { get; set; }
+        public string PASSWORD { get; set; }
+        public string NOMBRE { get; set; }
+        public string RUT { get; set; }
+        public string EMAIL { get; set; }
+
+
+
+    }
+}
