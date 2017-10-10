d5144932d1d0684cce68bbf4eda9fc9037cd2bc3
Jarys <Jarys@PORT>
2017-10-10 15:06:08 -0400

    s

--- Core.DALC/Core.Servicios/Service1.svc.cs
+++ Core.DALC/Core.Servicios/Service1.svc.cs
@@ -13,6 +13,7 @@ namespace Core.Servicios
     // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class Service1 : IService1
     {
+       
         public string ListarRegiones()
         {
             Negocio.Listas lista = new Negocio.Listas();
