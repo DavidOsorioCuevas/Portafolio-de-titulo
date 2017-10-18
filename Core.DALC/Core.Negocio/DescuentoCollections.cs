2d856c4a71090c50a1f1f4ec3202ea7e4abc1a9d
KlausTheEight <32248582+KlausTheEight@users.noreply.github.com>
2017-10-18 20:33:26 -0300

    Clases para mantenedores

---
+++ Core.DALC/Core.Negocio/DescuentoCollections.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Negocio
+{
+    public class DescuentoCollections: List<Descuento>
+    {
+        public DescuentoCollections() { }
+
+        public DescuentoCollections(string json)
+        {
+            DataContractJsonSerializer serializador = new DataContractJsonSerializer(typeof(DescuentoCollections));
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+
+            DescuentoCollections descList = (DescuentoCollections)serializador.ReadObject(stream);
+
+            this.AddRange(descList);
+        }
+
+        private DescuentoCollections GenerarLista(List<DALC.DESCUENTO> listaDALC)
+        {
+
+            DescuentoCollections list = new DescuentoCollections();
+            foreach (var item in listaDALC)
+            {
+                Descuento desc = new Descuento();
+
+                desc.IdDescuento = (int)item.ID_DESCUENTO;
+                desc.MinPuntos = (int)item.MIN_PUNTOS;
+                desc.MaxPuntos = (int)item.MAX_PUNTOS;
+                desc.FechaCaducidad = (DateTime)item.FECHA_CADUCIDAD;
+                desc.Porcentaje = (int)item.PORCENTAJE;
+                desc.Tope = (int)item.TOPE;
+
+                list.Add(desc);
+
+            }
+
+            return list;
+        }
+
+        public string ReadAllDescuentos()
+        {
+
+            var listaDA = new DALC.QueOfrecesEntities().DESCUENTO;
+            return GenerarLista(listaDA.ToList()).Serializar();
+
+        }
+
+        public string Serializar()
+        {
+
+            DataContractJsonSerializer serializador = new DataContractJsonSerializer(typeof(DescuentoCollections));
+            MemoryStream stream = new MemoryStream();
+
+            serializador.WriteObject(stream, this);
+
+            string ser = Encoding.UTF8.GetString(stream.ToArray());
+
+            return ser.ToString();
+
+        }
+    }
+}
---
+++ Core.DALC/Core.Negocio/RetailCollections.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Negocio
+{
+    public class RetailCollections: List<Retail>
+    {
+        public RetailCollections() { }
+
+        public RetailCollections(string json)
+        {
+            DataContractJsonSerializer serializador = new DataContractJsonSerializer(typeof(RetailCollections));
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+
+            RetailCollections retaList = (RetailCollections)serializador.ReadObject(stream);
+
+            this.AddRange(retaList);
+        }
+
+        private RetailCollections GenerarLista(List<DALC.RETAIL> listaDALC)
+        {
+
+            RetailCollections list = new RetailCollections();
+            foreach (var item in listaDALC)
+            {
+                Retail ret = new Retail();
+
+                ret.IdRetail = (int)item.ID_RETAIL;
+                ret.RutRetail = item.RUT;
+                ret.NombreRetail = item.NOMBRE;
+                ret.RazonSocial = item.RAZON_SOCIAL;
+                ret.Telefono = (int)item.TELEFONO;
+                ret.Email = item.EMAIL;
+                ret.Direccion = item.DIRECCION;
+                ret.IdRegion = (int)item.REGION_ID;
+                ret.IdComuna = (int)item.COMUNA_ID;
+                
+                list.Add(ret);
+
+            }
+
+            return list;
+        }
+
+        public string ReadAllRetail()
+        {
+
+            var listaRe = new DALC.QueOfrecesEntities().RETAIL;
+            return GenerarLista(listaRe.ToList()).Serializar();
+
+        }
+
+        public string Serializar()
+        {
+
+            DataContractJsonSerializer serializador = new DataContractJsonSerializer(typeof(RetailCollections));
+            MemoryStream stream = new MemoryStream();
+
+            serializador.WriteObject(stream, this);
+
+            string ser = Encoding.UTF8.GetString(stream.ToArray());
+
+            return ser.ToString();
+
+        }
+    }
+}
