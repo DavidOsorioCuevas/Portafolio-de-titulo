4c9f11eba8a013d675ebc5e23fd48f4ebd115a4e
Jarys <Jarys@DESKTOP-5GQVHGP>
2017-10-19 23:34:24 -0300

    Creación de método para crear usuarios via web, y validación de datos existentes

--- Core.DALC/Core.Servicios/IService1.cs
+++ Core.DALC/Core.Servicios/IService1.cs
@@ -15,6 +15,8 @@ namespace Core.Servicios
 
         /*USUARIO*/
         [OperationContract]
+        string CrearUsuarioWeb(string json);
+        [OperationContract]
         string ActivarUsuario(string json);
         [OperationContract]
         string ListarComunas();
