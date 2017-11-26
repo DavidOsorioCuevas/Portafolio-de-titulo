bf3ef0a83018b9060bf44e28bc27d7034fd57861
https://github.com/DavidOsorioCuevas/Portafolio-de-titulo <C:\temp\Repositorio>
2017-11-26 11:52:31 -0300

    EDM

--- Core.DALC/Core.DALC/EDMQueOfreces.Designer.cs
+++ Core.DALC/Core.DALC/EDMQueOfreces.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\}\Source\Repos\Portafolio-de-titulo3\Core.DALC\Core.DALC\EDMQueOfreces.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\temp\Repositorio\Portafolio-de-titulo\Core.DALC\Core.DALC\EDMQueOfreces.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
